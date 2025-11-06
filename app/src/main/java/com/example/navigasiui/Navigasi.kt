0d4a1ab01b8b28bb88bf094f2280de1a562893e7
Naufal0067 <naufalopang1928@gmail.com>
2025-11-06 12:23:17 +0700

    membuat scaffold isiRuang dan menambah form isian di kode

--- app/src/main/java/com/example/navigasiui/Navigasi.kt
+++ app/src/main/java/com/example/navigasiui/Navigasi.kt
@@ -1,10 +1,13 @@
 package com.example.navigasiui
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
 enum class Navigasi {
@@ -19,6 +22,19 @@ fun DataApp(
     modifier: Modifier
 ){
     Scaffold { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
 
+            modifier = Modifier.padding(isiRuang)){
+            composable(route = Navigasi.Formulirku.name){
+                FormIsian (
+                    //pilihanJK = JenisK .map { id -> konteks.resroude.getstring}
+                    OnSubmitBtnClick = {
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
+        }
     }
 }
